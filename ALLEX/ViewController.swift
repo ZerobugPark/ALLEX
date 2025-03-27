326cb57ced70f0d954f8f4dd990470102bc0e5fc
young <zerobugpark@gmail.com>
2025-03-27 13:20:39 +0900

    git init git ignore add

---
+++ ALLEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ALLEX
+//
+//  Created by youngkyun park on 3/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
