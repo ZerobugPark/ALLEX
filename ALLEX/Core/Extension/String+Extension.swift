63119fba60f3da588cde63648237d685666c13fe
young <zerobugpark@gmail.com>
2025-03-29 01:35:14 +0900

    다국어 파일 변경 (기존에 mac에 있는거 생성해서 수정)

---
+++ ALLEX/Core/Extension/String+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  String+Extension.swift
+//  ALLEX
+//
+//  Created by youngkyun park on 3/29/25.
+//
+
+import UIKit
+
+extension String {
+    
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    
+    //String(format)을 사용할 때, CVarArg 준수해야함
+    func localized<T: CVarArg>(with: T) -> String {
+        return String(format: self.localized, with)
+    }
+}
+
