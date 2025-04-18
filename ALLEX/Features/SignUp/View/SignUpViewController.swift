c8833c452294fc46f7db7fc85fa5e8cf3d2bb64b
young <zerobugpark@gmail.com>
2025-04-18 23:38:13 +0900

    chore: Sign Viwe Controlelr 최척화

--- ALLEX/Features/SignUp/View/SignUpViewController.swift
+++ ALLEX/Features/SignUp/View/SignUpViewController.swift
@@ -31,18 +31,10 @@ final class SignUpViewController: BaseViewController<SignUpView, SignUpViewModel
         
         let output = viewModel.transform(input: input)
         
-        output.nicknameLength.drive(with: self) { owner, value in
+        output.nicknameLength.drive(with: self) { owner, length in
             
-            owner.mainView.profile.countLabel.text = "\(value)/7"
-            
-            if value < 2 {
-                
-                owner.mainView.profile.infoLabel.text = LocalizedKey.Setting_UnVerified_NickName.rawValue.localized(with: "")
-            } else {
-                
-                owner.mainView.profile.infoLabel.text = LocalizedKey.Setting_Verified_NickName.rawValue.localized(with: "")
-            }
-            owner.mainView.profile.infoLabel.updateTextColorBasedOnLength(count: value)
+            owner.verifiedNickName(length: length)
+           
         }.disposed(by: disposeBag)
         
         
@@ -76,6 +68,26 @@ final class SignUpViewController: BaseViewController<SignUpView, SignUpViewModel
     
     
 }
+
+// MARK: ViewModel Setting Func
+extension SignUpViewController {
+    
+    private func verifiedNickName(length: Int) {
+        mainView.profile.countLabel.text = "\(length)/7"
+        
+        if length < 2 {
+            
+            mainView.profile.infoLabel.text = LocalizedKey.Setting_UnVerified_NickName.rawValue.localized(with: "")
+        } else {
+            
+            mainView.profile.infoLabel.text = LocalizedKey.Setting_Verified_NickName.rawValue.localized(with: "")
+        }
+        
+        mainView.profile.infoLabel.updateTextColorBasedOnLength(count: length)
+    }
+    
+}
+
 
 // MARK: - UITextFieldDelegate
 extension SignUpViewController: UITextFieldDelegate {
